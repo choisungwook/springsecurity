a6a330c3f3c2bc1a1a99bc39cd892aa85344a697
choisungwook <kgg1959@naver.com>
2021-10-10 19:54:34 +0900

    ession정보 출력 #1

---
+++ src/main/java/com/demo/springsecurity/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.demo.springsecurity.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/session")
+    public ResponseCookieDTO hello(@AuthenticationPrincipal User user, HttpSession HttpSession) {
+        String username = "empty";
+        String session_id = "empty";
+
+        if (user == null){
+            System.out.println("user is null");
+        } else {
+            session_id = HttpSession.getId();
+            username = user.getUsername();
+        }
+
+        return new ResponseCookieDTO(username, session_id);
+    }
+}
