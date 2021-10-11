7c4233e7d29750d1a27954f955f10a0d583bdd2d
choisungwook <kgg1959@naver.com>
2021-10-11 14:55:43 +0900

    로그인 성공,실패 핸들러 #3

--- backend/src/main/java/com/demo/springsecurity/config/LoginFailHandler.java
+++ backend/src/main/java/com/demo/springsecurity/config/LoginFailHandler.java
@@ -1,2 +1,20 @@
-package com.demo.springsecurity.config;public class LoginFailHandler {
+package com.demo.springsecurity.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class LoginFailHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+      log.error("로그인 실패");
+      httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+    }
 }
--- backend/src/main/java/com/demo/springsecurity/config/WebSecurityConfig.java
+++ backend/src/main/java/com/demo/springsecurity/config/WebSecurityConfig.java
@@ -40,6 +40,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .permitAll()
+                .failureHandler(new LoginFailHandler())
+                .successHandler(new LoginSuccessHandler())
                 .and()
             .logout()
                 .permitAll()
